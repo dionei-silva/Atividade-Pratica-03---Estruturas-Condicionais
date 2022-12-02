0e630d6f048aee8cb45d07e4f2aa89d835216cbd
Dionei Silva <dionei.silva@gmail.com>
2022-12-02 11:40:42 -0300

    exrecicio4-ok

--- exercicio4/script.js
+++ exercicio4/script.js
@@ -1,3 +1,15 @@
 /* 4. Escreva um algoritmo que percorra os números inteiros entre 1 e 250
 e verifique quais números são múltiplos de 3 e quais são múltiplos
 de 5. */
+
+let contador = 1;
+
+while (contador <= 250) {
+    if (contador % 3 == 0) {
+        document.write(`valor multiplo de 3: ${contador}<br/>`);
+    }
+    if (contador % 5 == 0) {
+        document.write(`valor multiplo de 5: ${contador}<br/>`);
+    }
+    contador++;
+}
