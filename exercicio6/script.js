571577b0a81f0d3da61ce7a9681c38e45c73c911
Dionei Silva <dionei.silva@gmail.com>
2022-12-02 15:17:38 -0300

    exercicio6-on

--- exercicio6/script.js
+++ exercicio6/script.js
@@ -4,3 +4,31 @@ Construa um algoritmo que calcule e imprima quantos anos serão
 necessários para que:
 a. Lucas e Pedro tenham o mesmo tamanho;
 b. Lucas seja maior que Pedro. */
+
+let pedro = 150;
+let lucas = 110;
+
+let ano = 0;
+
+while (pedro >= lucas) {
+    pedro += 2;
+    lucas += 3;
+
+    ano++;
+
+    console.log(`pedro ${pedro}`);
+    console.log(`lucas ${lucas}`);
+    console.log(`anos ${ano}`);
+
+    if (lucas === pedro) {
+        console.log(
+            `levou ${ano} anos para lucas chegar na mesma altura que pedro`
+        );
+    }
+
+    if (lucas > pedro) {
+        console.log(
+            `levou ${ano} anos para lucas ultrapassar a mesma altura de pedro`
+        );
+    }
+}
