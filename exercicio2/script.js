c64b4a19fbc8336f7f3eba8e53ff3a5f8878422d
Dionei Silva <dionei.silva@gmail.com>
2022-12-02 06:11:29 -0300

    exercicio2-ok

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,3 +1,36 @@
 /* 2. Escreva um algoritmo que solicite 2 números e uma operação
 matemática. O algoritmo deve realizar o cálculo com os 2 números
 digitados conforme a operação informada. */
+
+let numero1 = Number(prompt("insira um numero: "));
+let numero2 = Number(prompt("insira um numero: "));
+let operação = window.prompt(
+    "insira a operação a ser realizada: +,-,/,*,%,ou **"
+);
+
+switch (operação) {
+    case "+":
+        document.write(`Resultado é ${numero1 + numero2}`);
+        break;
+
+    case "-":
+        document.write(`Resultado é ${numero1 - numero2}`);
+        break;
+
+    case "/":
+        document.write(`Resultado é ${numero1 / numero2}`);
+        break;
+    case "*":
+        document.write(`Resultado é ${numero1 * numero2}`);
+        break;
+    case "%":
+        document.write(`Resultado é ${numero1 % numero2}`);
+        break;
+    case "**":
+        document.write(`Resultado é ${numero1 ** numero2}`);
+        break;
+
+    default:
+        document.write("operador invalido");
+        break;
+}
