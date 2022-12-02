ce8d33cbfd7c1b513465bb68b0c89b4555a85dfc
Dionei Silva <dionei.silva@gmail.com>
2022-12-02 05:58:06 -0300

    exercicio1-ok

---
+++ exercicio1/script.js
@@ -0,0 +1,36 @@
+/* 1. Desenvolva um algoritmo que faça o cálculo do índice de massa
+corporal de uma pessoa. Esse algoritmo deve solicitar ao usuário
+que digite sua altura, em seguida solicitar que digite seu peso e que
+lhe exiba o status. O status vai variar da seguinte forma:
+a. Se o IMC for menor que 18,5 o status a ser mostrado deve ser
+“Você está abaixo da faixa de peso ideal”;
+b. Se o IMC estiver acima de 24,99 o status a ser mostrado será
+“Você está acima da faixa de peso ideal”;
+c. Se o IMC for igual ou maior que 18,5 e igual ou menor que
+24,99, o status a ser mostrado será “Você está dentro da faixa
+de peso ideal”. */
+
+let altura = window.prompt("digite sua altura ex: 1.71 : ");
+if (altura !== null) {
+    altura = Number(altura.replace(",", "."));
+}
+
+if (!altura) {
+    document.write("altura invalida");
+} else {
+    let peso = Number(prompt("digite seu peso: "));
+    if (!peso) {
+        document.write("peso invalido");
+    } else {
+        let imc = peso / (altura * altura);
+        document.write(`${imc.toFixed(2)}`);
+        if (imc <= 18.5) {
+            document.write(" Você está abaixo da faixa de peso ideal");
+        } else if (imc > 24.99) {
+            document.write(" Você está acima da faixa de peso ideal");
+        } else {
+            imc >= 18.5 && imc <= 24.99;
+            document.write(" Você está dentro da faixa de peso ideal");
+        }
+    }
+}
