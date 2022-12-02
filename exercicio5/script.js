6d496c40b6b37b51037bff9808569697e452aef9
Dionei Silva <dionei.silva@gmail.com>
2022-12-02 07:50:25 -0300

    exercicio5-ok

--- exercicio5/script.js
+++ exercicio5/script.js
@@ -9,3 +9,33 @@ Acima de 40kg Juvenil pesado
 Acima de 45kg até 60kg Sênior médio
 Acima de 60kg Sênior pesado
 Acima de 24 anos - Veterano */
+
+let peso = prompt("insira seu peso em kg: ");
+if (peso !== null) {
+    peso = Number(peso.replace(".", "").replace(",", "."));
+}
+if (!peso) {
+    document.write("peso invalido");
+} else {
+    let idade = prompt("insira sua idade em anos: ");
+    if (idade !== null) {
+        idade = Number(idade.replace(".", "").replace(",", "."));
+    }
+    if (!idade) {
+        document.write("idade invalida");
+    } else {
+        if (idade < 12) {
+            document.write("infantil");
+        } else if (idade <= 16 || peso < 40) {
+            document.write("juvenil leve");
+        } else if (peso > 40) {
+            document.write("juvenil pesado");
+        } else if (idade <= 24 || peso < 45) {
+            document.write("senior leve");
+        } else if (peso < 60) {
+            document.write("senior medio");
+        } else if (peso > 60) {
+            document.write("senior pesado");
+        } else document.write("veterano");
+    }
+}
