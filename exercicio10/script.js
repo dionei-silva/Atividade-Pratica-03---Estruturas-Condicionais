e2831bf56efb9714612d8d320e5bb9d120f3f5e6
Dionei Silva <dionei.silva@gmail.com>
2022-12-06 06:09:36 -0300

    exercicio10-ok

--- exercicio10/script.js
+++ exercicio10/script.js
@@ -3,3 +3,9 @@ inteiro positivo e mostre na tela uma contagem de 0 até o valor
 digitado:
 Ex: Digite um valor: 9
 Contagem: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, FIM! */
+
+let numero = Number(prompt("Digite numero: "));
+for (let contador = 0; contador <= numero; contador++) {
+    document.write(`${contador}, `);
+}
+document.write("FIM!");
