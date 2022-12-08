b1fc6314e71d619e6e9b5f42837cba38f17ab613
Dionei Silva <dionei.silva@gmail.com>
2022-12-08 16:07:28 -0300

    exercicio11-ok

--- exercicio11/script.js
+++ exercicio11/script.js
@@ -2,3 +2,31 @@
 30 até 1, marcando os números que forem primos, exatamente
 como mostrado abaixo:
 30 [29] 28 27 26 25 24 [23] 22 21 20 [19] 18 [17] 16... */
+function exercicio11() {
+    for (let contador = 30; contador >= 1; contador--) {
+        const primo = verificaPrimo(contador);
+
+        if (primo) {
+            document.write(` [${contador}] `);
+        } else {
+            document.write(` ${contador} `);
+        }
+    }
+}
+
+function verificaPrimo(numero) {
+    // primo so pode ter 2 divisores o numero 1 e ele mesmo
+    let divisores = 2; // começamos com 2 pois todo numero e dividido por 1 e ele mesmo
+
+    for (let contador = 2; contador < numero; contador++)
+        // ex: 7
+        // 2 a 6
+        if (numero % contador === 0) {
+            divisores++;
+        }
+    if (divisores > 2) {
+        return false;
+    } else {
+        return true;
+    }
+}
